808e2bd8c7bfbf74fb21a9c3975c5d45f2dacb85
Russell Scheerer <russell.scheerer@gmail.com>
2017-01-01 09:24:53 -0500

    Add RabbitMQ for chat relay

--- src/main/java/com/indysfug/ChatHandler.java
+++ src/main/java/com/indysfug/ChatHandler.java
@@ -1,6 +1,14 @@
 package com.indysfug;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -16,7 +24,17 @@ import java.util.List;
 @Slf4j
 public class ChatHandler {
 
+    public static final String CHAT_MESSAGE_TOPIC = "chat.messages";
+
     private final List<SseEmitter> emitters = Collections.synchronizedList(new ArrayList<>());
+    private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public ChatHandler(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.objectMapper = objectMapper;
+    }
 
     public void addEmitter(SseEmitter emitter) {
         log.info("Adding emitter: {}", emitter.toString());
@@ -25,7 +43,18 @@ public class ChatHandler {
     }
 
     public void publishMessage(Message message) throws Exception {
-        broadcastToSse(new ChatMessageEvent(message));
+        String data = objectMapper.writeValueAsString(new ChatMessageEvent(message));
+        rabbitTemplate.convertAndSend(CHAT_MESSAGE_TOPIC, "", data);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue,
+            exchange = @Exchange(value = CHAT_MESSAGE_TOPIC, ignoreDeclarationExceptions = "true", type = ExchangeTypes.TOPIC)))
+    public void onChatMessageEvent(String message) throws Exception {
+        ChatMessageEvent chatMessageEvent = objectMapper.readValue(message, ChatMessageEvent.class);
+
+        log.info("Received ChatMessageEvent from topic: {}", chatMessageEvent);
+        broadcastToSse(chatMessageEvent);
     }
 
     protected void broadcastToSse(ChatMessageEvent chatMessageEvent) {
